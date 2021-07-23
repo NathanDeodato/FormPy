823cf654024845c40731fce2354aa89bc1aa5866
NathanDeodato <ndeodato42@outlook.com>
2021-07-23 11:47:25 -0300

    Update Form-v2

--- Form-v2.py
+++ Form-v2.py
@@ -7,29 +7,23 @@ from time import sleep
 def FormPy():
     sg.theme("DarkTeal7")
     layout = [
-        [sg.Text("First Name |", text_color="black", size=(9, 0)), sg.Input(key="firstname", size=(20, 0), border_width=3, justification="center")],
-        [sg.Text("Last Name |", text_color="black", size=(9, 0)), sg.Input(key="lastname", size=(20, 0), border_width=3,justification="center")],
-        [sg.Text("Age |", text_color="black", size=(9, 0)), sg.Input(key="age", size=(8, 0), border_width=3, justification="center")],
-
-        [sg.Text("-" * 66)],
+        [sg.Text("First Name |", text_color="black", size=(9, 0)), sg.Input(key="firstname", size=(20, 0), border_width=3, justification="center"), sg.Text("|"), sg.Text("Country |", text_color="black", size=(7, 0)), sg.Input(key="country", size=(15, 0), border_width=3,justification="center")],
+        [sg.Text("Last Name |", text_color="black", size=(9, 0)), sg.Input(key="lastname", size=(20, 0), border_width=3,justification="center"), sg.Text("|"), sg.Text("State |", text_color="black", size=(7, 0)), sg.Input(key="state", size=(15, 0), border_width=3,justification="center")],
         
-        [sg.Text("Identity |", text_color="black", size=(9, 0)), sg.Input(key="identity", size=(20, 0), border_width=3, justification="center")],
-        [sg.Text("License |", text_color="black", size=(9, 0)), sg.Input(key="license", size=(20, ), border_width=3, justification="center")],
+        [sg.Text("-" * 111)],
 
-        [sg.Text("-" * 66)],
-
-        [sg.Text("Email |", text_color="black", size=(9, 0)), sg.Input(key="email", size=(25, 0), border_width=3, justification="center")],
-        [sg.Text("Number |", text_color="black", size=(9, 0)), sg.Input(key="number", size=(15, 0), border_width=3, justification="center")],
+        [sg.Text("Identity |", text_color="black", size=(9, 0)), sg.Input(key="identity", size=(20, 0), border_width=3, justification="center"), sg.Text("|"), sg.Text("Email |", text_color="black", size=(7, 0)), sg.Input(key="email", size=(15, 0), border_width=3, justification="center")],
+        [sg.Text("License |", text_color="black", size=(9, 0)), sg.Input(key="license", size=(20, ), border_width=3, justification="center"), sg.Text("|"), sg.Text("Number |", text_color="black", size=(7, 0)), sg.Input(key="number", size=(15, 0), border_width=3, justification="center")],
         
-        [sg.Text("-" * 66)],
+        [sg.Text("-" * 111)],
 
-        [sg.Text(" " * 23), sg.Button("Register", size=(6, 0))],
+        [sg.Text(" " * 46), sg.Button("Register", size=(6, 0))],
 
-        [sg.Text("-" * 66)],
+        [sg.Text("-" * 111)],
 
-        [sg.Output(size=(35, 5))],
+        [sg.Output(size=(61, 6))],
         
-        [sg.Text(" " * 18), sg.Text("(c) NT Developer", text_color="black",)]
+        [sg.Text(" " * 42), sg.Text("(c) NT Developer", text_color="black",)]
     ]
 
     return sg.Window("FormPy", layout=layout)
@@ -47,7 +41,9 @@ while True:
         # Dates
         firstname = str(values["firstname"]).capitalize()
         lastname = str(values["lastname"]).capitalize()
-        age = int(values["age"])
+        
+        country = str(values["country"]).capitalize()
+        state = str(values["state"]).capitalize()
 
         identity = str(values["identity"])
         license = str(values["license"])
@@ -62,12 +58,13 @@ while True:
                 code = 0
                 code += 1
                 
-                print("-" * 60)
+                print("-" * 106)
+                sleep(1.5)
 
-                print("                  - REGISTERED! -")
-                print(f"NameCode - #{code}_#{firstname}{lastname}")
+                print("                                             - REGISTERED! -")
+                print(f"                                    NameCode - #{code}_#{firstname}{lastname}")
 
-                print("-" * 60)
+                print("-" * 106)
                 print(" ")
 
         # Defs
